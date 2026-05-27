59acd773d599e456d83d0fc3cfebb0ea6f59e4fe
Ahmed <almkhtvt@gmail.com>
2026-05-27 14:36:40 +0300

    fix build system and stable CI

--- setup.py
+++ setup.py
@@ -3,19 +3,18 @@ from setuptools import setup, Extension
 
 include_dirs = ["include"]
 
-extra_compile_args = []
-extra_link_args = []
-
-libraries = []
-
 if sys.platform == "win32":
-    libraries = ["crypt32"]
+    libraries = ["libssl", "libcrypto", "zlib"]
+    extra_compile_args = ["/O2"]
+    extra_link_args = []
 elif sys.platform == "darwin":
     libraries = ["ssl", "crypto", "z"]
     extra_compile_args = ["-O3"]
+    extra_link_args = []
 else:
     libraries = ["ssl", "crypto", "z"]
     extra_compile_args = ["-O3"]
+    extra_link_args = []
 
 epy_runtime_module = Extension(
     "epy_runtime",
@@ -38,7 +37,6 @@ epy_runtime_module = Extension(
 setup(
     name="epy_runtime",
     version="1.0.0",
-    description="Stable Python Runtime Extension",
     ext_modules=[epy_runtime_module],
     py_modules=["builder"],
     package_dir={"": "python"},
